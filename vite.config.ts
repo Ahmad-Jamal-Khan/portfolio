30c6ddaad36e639220cf94c0edf9cc87b9b59e10
Ahmad Jamal Khan <engr.ahmadjamalkhan@gmail.com>
2025-04-15 00:05:49 +0500

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,22 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
   base: '/Portfolio/', // Replace with your repository name
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
 }) 
