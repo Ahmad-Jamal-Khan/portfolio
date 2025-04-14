a130c79c52b277b94ec4798ee41ab9220c1295d3
Ahmad Jamal Khan <engr.ahmadjamalkhan@gmail.com>
2025-04-15 00:13:36 +0500

    committ

--- vite.config.ts
+++ vite.config.ts
@@ -21,5 +21,10 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
 }) 
